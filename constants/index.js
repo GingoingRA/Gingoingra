ce422f4cc027944b5bc6362b417e148a4574470f
GingoingRA <95692591+GingoingRA@users.noreply.github.com>
2023-08-01 07:46:07 -1200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x1ab441fa2f0dfd5821e37195e4f5af5675cc8806";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
